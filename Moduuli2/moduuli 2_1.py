9993406c5413b77613690d8b0af767641ff18a5b
Koti <daauud.ismail@gmail.com>
2025-01-17 10:12:12 +0200

    testi

---
+++ Moduuli2/moduuli 2_1.py
@@ -0,0 +1,7 @@
+print("hello world")
+print("tämä on toinen tulostusrivi")
+print ("ja kolmas...")
+print("jne")
+
+käyttäjä = input('Anna nimesi: Daauud')
+           print("Hauska tavata,"Daauud" "!")
