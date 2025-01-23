fed37f4e3de6fb0fd0062c8644875cf6449af879
daauudi <daauud.ismail@gmail.com>
2025-01-23 10:26:24 +0200

    Update moduuli2_6.py

--- Moduuli2/moduuli2_6.py
+++ Moduuli2/moduuli2_6.py
@@ -1,11 +1,8 @@
 import random
 
-# Kolmenumeroinen koodi (numerot 0..9)
 kolmenumeroinen_koodi = ''.join(str(random.randint(0, 9)) for _ in range(3))
 
-# Nelinumeroinen koodi (numerot 1..6)
 nelinumerooinen_koodi = ''.join(str(random.randint(1, 6)) for _ in range(4))
 
-# Tulostetaan molemmat koodit
 print(f"Kolmenumeroinen koodi: {kolmenumeroinen_koodi}")
 print(f"Nelinumeroinen koodi: {nelinumerooinen_koodi}")
